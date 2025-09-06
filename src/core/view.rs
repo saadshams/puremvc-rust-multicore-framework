0b7c1f2f3ba17e5f431e0b86f8bff398a51dfd06
Saad <saadmuizz@gmail.com>
2025-09-06 13:22:03 -0400

    update

--- src/core/view.rs
+++ src/core/view.rs
@@ -87,7 +87,7 @@ impl IView for View {
 
         if let Some(mediator) = &removed {
             let interests = mediator.lock().unwrap().list_notification_interests();
-            let context = Arc::new(Box::new(mediator.clone()) as Box<dyn Any + Send + Sync>);
+            let context: Arc<Box<dyn Any + Send + Sync>> = Arc::new(Box::new(mediator.clone()));
             for interest in interests {
                 self.remove_observer(&interest, &context);
             }
