cc16bcd809f5cebcfb5bf502d02be53a81dae666
Saad <saadmuizz@gmail.com>
2025-09-13 14:36:09 -0400

    update

--- src/core/mod.rs
+++ src/core/mod.rs
@@ -1,8 +1,3 @@
-mod controller;
 pub use controller::Controller;
-
-mod model;
 pub use model::Model;
-
-mod view;
 pub use view::View;
--- src/interfaces/mod.rs
+++ src/interfaces/mod.rs
@@ -1,29 +1,10 @@
-mod icontroller;
 pub use icontroller::IController;
-
-mod imodel;
 pub use imodel::IModel;
-
-mod iview;
 pub use iview::IView;
-
-mod icommand;
 pub use icommand::ICommand;
-
-mod ifacade;
 pub use ifacade::IFacade;
-
-mod imediator;
 pub use imediator::IMediator;
-
-mod inotification;
 pub use inotification::INotification;
-
-mod inotifier;
 pub use inotifier::INotifier;
-
-mod iobserver;
 pub use iobserver::IObserver;
-
-mod iproxy;
 pub use iproxy::IProxy;
