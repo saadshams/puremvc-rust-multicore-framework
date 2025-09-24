ed5a934f4c02fcd3e0b8392a6600711ea27efe25
Abubakar Ummar <haywayaheadshotstrategy.fx@gmail.com>
2025-09-24 20:48:04 +0000

    Fix typo for test case

--- tests/core/controller_test.rs
+++ tests/core/controller_test.rs
@@ -165,7 +165,7 @@ fn test_register_and_execute_command() {
     controller.execute_command(&(notification as Arc<dyn INotification>));
 
     // Assert that the result is input * 2 (12 * 2 = 24)
-    assert_eq!(vo.read().unwrap().result, 24);
+    assert_eq!(vo.write().unwrap().result, 24);
 }
 
 /// Tests Command registration and removal.
@@ -254,5 +254,5 @@ fn test_reregister_and_execute_command() {
     view.notify_observers(&notification);
 
     // Assert that the result is input * 2 (12 * 2 = 24), confirming single execution
-    assert_eq!(vo.write().unwrap().result, 24);
+    assert_eq!(vo.read().unwrap().result, 24);
 }
